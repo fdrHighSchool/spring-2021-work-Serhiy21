41e2a05d13cf5cd99213906ad5099522b2e24ba3
Serhiy21 <72924537+Serhiy21@users.noreply.github.com>
2021-03-20 20:14:38 -0400

    Create GameofLifeDriver.java

---
+++ 02-Gameoflife/GameofLifeDriver.java
@@ -0,0 +1,18 @@
+public class GameofLifeDriver{
+  public static void main(String[] args) {
+    GameofLife board1 = new GameofLife();
+    GameofLife board2 = new GameofLife(20,20);
+  
+
+    board2.randomBoard();
+    board2.printBoard();
+
+    board2.updateBoard();
+
+    board2.printBoard();
+    board2.updateBoard();
+
+    board2.printBoard();
+  }
+
+}
